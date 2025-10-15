a4a88f570fa89047b1122d366706907aead0d956
GusF <gustavof@tinet.fut.es>
2025-10-15 13:40:25 +0200

    Commit inicial

---
+++ src/components/custom/CustomMenu.tsx
@@ -0,0 +1,33 @@
+import { Link, useLocation } from "react-router"
+import { NavigationMenu, NavigationMenuList } from "../ui/navigation-menu"
+import { cn } from "@/lib/utils";
+import { NavigationMenuItem, NavigationMenuLink } from "@radix-ui/react-navigation-menu";
+
+export const CustomMenu = () => {
+  
+  const { pathname } = useLocation();
+  
+  const isActive = (path: string) => {
+    return pathname === path;
+  }
+  
+  return (
+    <NavigationMenu className="py-5">
+        <NavigationMenuList>
+            {/* Home */}
+            <NavigationMenuItem>
+                <NavigationMenuLink asChild className={cn(isActive('/') && 'bg-slate-200 rounded-md', 'p-2')}>
+                    <Link to="/">Home</Link>
+                </NavigationMenuLink>
+            </NavigationMenuItem>
+            
+            {/* Search */}
+            <NavigationMenuItem>
+                <NavigationMenuLink asChild className={cn(isActive('/search') && 'bg-slate-200 rounded-md', 'p-2')}>
+                    <Link to="/search">Buscar Superhéroe</Link>
+                </NavigationMenuLink>
+            </NavigationMenuItem>
+        </NavigationMenuList>
+    </NavigationMenu>
+  )
+}
